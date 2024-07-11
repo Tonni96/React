86611c441c60bd92804d347369faf9f01984af0f
Tonni96 <antonigso96@gmail.com>
2024-07-10 18:55:05 -0600

    commit

--- src/Component/productCard/productcard.jsx
+++ src/Component/productCard/productcard.jsx
@@ -4,6 +4,6 @@ export const ProductCard = ({ title, description, price }) => {
             <h2>{title}</h2>
             <h3>{description}</h3>
             <h3>{price}</h3>
-            </div>
-    );
-    }; 
+        </div>
+        );
+    };
