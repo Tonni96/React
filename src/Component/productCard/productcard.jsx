c4b63261216b03db0bda96d8e5803e523c74f222
Tonni96 <antonigso96@gmail.com>
2024-07-22 16:09:50 -0600

    CAMBIOS PUTO

--- src/Component/productCard/productcard.jsx
+++ src/Component/productCard/productcard.jsx
@@ -1,30 +1,9 @@
-import { Button, Card, CardActions, CardContent, CardMedia, Typography } from "@mui/material";
-import { Link } from "react-router-dom";
-
-export const ProductCard = ({ title, description, price, id, img}) => {
+export const ProductCard = ({ title, description, price }) => {
     return (
-        <Card sx={{ maxWidth: 345 }}>
-      <CardMedia
-        sx={{ height: 140, width: "100%"}}
-        image= {img}
-        title="green iguana"
-      /> 
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-            <div>{title}</div>
-        </Typography>
-        <Typography variant="body2" color="Black">
-            <div>{description}</div>
-        </Typography>
-        <Typography variant="body2" color="Black">
-          <div>${price}</div>
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Link to={`/itemDetail/${id}`}>
-        <Button size="small">Ver mas </Button>
-        </Link>
-      </CardActions>
-    </Card>
+        <div style={{ border: "2px solid steelblue" }}>
+            <h2>{title}</h2>
+            <h3>{description}</h3>
+            <h3>{price}</h3>
+        </div>
         );
     };
